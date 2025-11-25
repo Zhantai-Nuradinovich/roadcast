ea469e17187bb7939db0d235827760d336c4dd6c
zhanta <jantai.nuradinovich@gmail.com>
2025-11-25 12:49:20 +0600

    Add migrations

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # roadcast
 Main roadcast repository (MVP now, will be splitted into microservices if needed)
+
+## Migrations
+- dotnet ef migrations add Initial --context ApplicationDbContext --startup-project src/roadcast.Api/roadcast.Api.csproj --project src/roadcast.Persistence/roadcast.Persistence.csproj --output-dir Migrations/001
---
+++ src/roadcast.Persistence/ApplicationDbContextFactory.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace roadcast.Persistence;
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    private const string ConnectionStringName = "DatabaseConnection";
+    private const string AspNetCoreEnvironment = "ASPNETCORE_ENVIRONMENT";
+
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        optionsBuilder.UseNpgsql(GetConnectionString());
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+
+    private static string GetConnectionString()
+    {
+        var basePath = Directory.GetCurrentDirectory();
+
+        var environmentName = Environment.GetEnvironmentVariable(AspNetCoreEnvironment);
+        Console.WriteLine(environmentName);
+
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(basePath)
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile($"appsettings.Local.json", optional: true)
+            .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        Console.WriteLine(configuration.GetConnectionString(ConnectionStringName));
+
+        var connectionString = configuration.GetConnectionString(ConnectionStringName);
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new ArgumentException($"Connection string '{ConnectionStringName}' is null or empty.", nameof(connectionString));
+        }
+
+        return connectionString;
+    }
+}
